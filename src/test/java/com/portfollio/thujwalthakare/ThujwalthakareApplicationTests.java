155ba9713d7756029be7544caa415e19731c70a2
thaka <thakre2829@gmail.com>
2025-02-14 00:01:07 +0530

    adding project files

---
+++ src/test/java/com/portfollio/thujwalthakare/ThujwalthakareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfollio.thujwalthakare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThujwalthakareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
